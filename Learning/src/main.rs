26b60cf47fb9525ef32de3529e547008e8da8808
PPenmetsa <ppenmet@ncsu.edu>
2020-08-24 16:55:01 -0400

    Create main.rs

---
+++ Learning/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Printing on a new line:
+    println!("Hello, world!");
+
+    // Initializing a mutable variable as integer:
+    let mut a: i32 = 7;
+    println!("The variable a is: {}", a);
+}
